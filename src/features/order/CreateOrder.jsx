da9e46abb435ca1aebb108f9f6b18c026c6a7822
samriddhi <samriddhi.jagriti@gmail.com>
2026-01-13 23:43:12 +0530

    update

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import { useSelector } from 'react-redux';
+import { clearCart } from '../cart/cartSlice';
+import store from '../../store';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -10,39 +11,13 @@ const isValidPhone = (str) =>
     str,
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
   const navigation = useNavigation();
   const username = useSelector((state) => state.user.username);
+  const cart = useSelector((state) => state.cart.cart);
   const isSubmitting = navigation.state === 'submitting';
 
   const formErrors = useActionData();
-
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
 
   return (
     <div className="px-4 py-6">
@@ -132,11 +107,12 @@ export async function action({ request }) {
   if (Object.keys(errors).length > 0) return errors;
   // redirect fn by react router. Cant useNavigate here as it is a hook, this returns a response. so browser redirects
 
-  // const newOrder = await createOrder(order);
+  const newOrder = await createOrder(order);
 
-  // return redirect(`/order/${newOrder.id}`);
+  // Clear cart after successful order
+  store.dispatch(clearCart());
 
-  return null;
+  return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
